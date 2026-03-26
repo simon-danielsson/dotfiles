8613dadd46835523613c439b839ecb54ca8a36d6
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-03-26 19:16:48 +0100

    update

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -22,89 +22,121 @@ vim.lsp.config('rust_analyzer', {
         ['rust-analyzer'] = {},
     },
 })
-
 vim.lsp.enable('rust_analyzer')
 
+-- python
+vim.lsp.config('pyright', {
+    cmd = { 'pyright-langserver', '--stdio' },
+    filetypes = { 'python' },
+    root_markers = {
+        'pyproject.toml',
+        'setup.py',
+        '.git',
+    },
+    settings = {
+        python = {
+            analysis = {
+                typeCheckingMode = "basic", -- or "strict"
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+            },
+        },
+    },
+})
+vim.lsp.enable('pyright')
+
 -- -- c/c++
--- vim.lsp.enable('clangd')
+vim.lsp.config('clangd', {
+    cmd = {
+        'clangd',
+        '--background-index',
+        '--clang-tidy',
+        '--completion-style=detailed',
+        '--header-insertion=iwyu',
+    },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+    root_markers = {
+        'compile_commands.json',
+        'compile_flags.txt',
+        '.git',
+    },
+})
+vim.lsp.enable('clangd')
 
 -- -- gdscript
 -- vim.lsp.enable('gdscript')
 
--- -- python
--- vim.lsp.enable('pyright')
-
--- -- js/ts
--- vim.lsp.enable('ts_ls')
+-- js/ts
+vim.lsp.config('tsserver', {
+    cmd = { 'typescript-language-server', '--stdio' },
+    filetypes = {
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+    },
+    root_markers = { 'package.json', 'tsconfig.json', '.git' },
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "all",
+            },
+        },
+        javascript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "all",
+            },
+        },
+    },
+})
+vim.lsp.enable('tsserver')
 
 -- lua
--- vim.lsp.config('lua_ls', {
-    --     root_markers = {
-        --         { '.luarc.json',  '.luarc.jsonc' },
-        --         { '.stylua.toml', 'stylua.toml' },
-        --         { 'selene.toml',  'selene.yml' },
-        --         '.git',
-        --     },
-        --
-        --     root_dir = function(bufnr, on_dir)
-            --         local fname = vim.api.nvim_buf_get_name(bufnr)
-            --         local nvim_config = vim.fn.stdpath('config')
-            --         local home = vim.env.HOME
-            --
-            --         if fname:sub(1, #nvim_config) == nvim_config then
-            --             on_dir(nvim_config)
-            --             return
-            --         end
-            --
-            --         local root = vim.fs.root(fname, {
-                --             '.luarc.json',
-                --             '.luarc.jsonc',
-                --             '.stylua.toml',
-                --             'stylua.toml',
-                --             'selene.toml',
-                --             'selene.yml',
-                --             '.git',
-                --         })
-                --
-                --         if root and root ~= home then
-                --             on_dir(root)
-                --             return
-                --         end
-                --
-                --         on_dir(vim.fs.dirname(fname))
-                --     end,
-                --
-                --     settings = {
-                    --         Lua = {
-                        --             runtime = {
-                            --                 version = 'LuaJIT',
-                            --             },
-                            --             workspace = {
-                                --                 checkThirdParty = false,
-                                --                 library = vim.api.nvim_get_runtime_file('', true),
-                                --             },
-                                --         },
-                                --     },
-                                -- })
-                                -- vim.lsp.enable('lua_ls')
+vim.lsp.config('lua_ls', {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = { '.git', '.luarc.json', '.luarc.jsonc' },
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = { 'vim' }, -- rec nvim api
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                },
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+vim.lsp.enable('lua_ls')
 
-                                -- -- css
-                                -- vim.lsp.config('cssls', {
-                                    --     cmd = { 'vscode-css-language-server', '--stdio' },
-                                    --     capabilities = capabilities,
-                                    -- })
-                                    -- vim.lsp.enable('cssls')
-                                    --
-                                    -- -- html
-                                    -- vim.lsp.config('html', {
-                                        --     cmd = { 'vscode-html-language-server', '--stdio' },
-                                        --     capabilities = capabilities,
-                                        -- })
-                                        -- vim.lsp.enable('html')
-                                        --
-                                        -- -- spell-checking
-                                        -- vim.lsp.config['harper'] = {
-                                            --     cmd = { 'harper-ls', '--stdio' },
-                                            --     filetypes = { 'markdown', 'text', 'tex', 'typst' }
-                                            -- }
-                                            -- vim.lsp.enable('harper')
+-- css
+vim.lsp.config('cssls', {
+    cmd = { 'vscode-css-language-server', '--stdio' },
+    filetypes = { 'css', 'scss', 'less' },
+    capabilities = capabilities,
+})
+vim.lsp.enable('cssls')
+
+-- html
+vim.lsp.config('html', {
+    cmd = { 'vscode-html-language-server', '--stdio' },
+    filetypes = { 'html' },
+    capabilities = capabilities,
+})
+vim.lsp.enable('html')
+
+-- spell-checking
+vim.lsp.config['harper'] = {
+    cmd = { 'harper-ls', '--stdio' },
+    filetypes = { 'markdown', 'text', 'tex', 'typst' }
+}
+vim.lsp.enable('harper')
