2ee85529b0c713bbdd5a44b620070562ff8a658d
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-06-23 08:34:47 +0200

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,7 +13,6 @@ require('modules').setup()
 -- plugins --------------------------------------------------------------------
 
 vim.opt.rtp:prepend("~/.config/nvim/plugins/render-markdown.nvim")
-
 require('render-markdown').setup({
     completions = { lsp = { enabled = true } },
 })
