2d7349e683d467cb6d479911e463886601700512
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-07-21 15:25:09 +0200

    update

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -263,7 +263,7 @@ function M.setup()
             elseif ft == "bash" then
                 cmd("normal! gg=G")
             elseif ft == "python" then
-                cmd("silent! %!black -q -")
+                cmd("silent! %!black --fast -q -")
                 local pos = vim.api.nvim_win_get_cursor(0)
                 cmd([[silent! %s/\s\+$//e]]); cmd([[silent! %s/\(\n\)\{3,}/\r\r/e]])
                 vim.api.nvim_win_set_cursor(0, pos)
